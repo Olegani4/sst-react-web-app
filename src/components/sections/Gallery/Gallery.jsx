b09f56db24308852bb23c6b967efcaa538c9fa78
Olegani4 <thezymm12@hotmail.com>
2025-07-22 21:55:52 +0300

    feat: gallery section

--- src/components/sections/Gallery/Gallery.jsx
+++ src/components/sections/Gallery/Gallery.jsx
@@ -1,9 +1,63 @@
+import { useEffect } from 'react';
+import lightbox from 'lightbox2';
+import 'lightbox2/dist/css/lightbox.min.css';
+import { galleryImages } from '../../../utils/gallery';
+
 function Gallery() {
+    useEffect(() => {
+        // Initialize Lightbox
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true,
+            'albumLabel': 'Image %1 of %2',
+            'fadeDuration': 300,
+            'imageFadeDuration': 300,
+            'disableScrolling': true,
+            'fitImagesInViewport': true,
+            'positionFromTop': 100,
+        });
+    }, []);
+
     return (
         <section className="gallery" id="gallery-section">
-            Gallery section
+            <div className="gallery__container">
+                <div className="gallery__header">
+                    <h2 className="gallery__title heading-1">Space Gallery</h2>
+                </div>
+                
+                <div className="gallery__content">
+                    <p className="gallery__subtitle body-large">Explore our amazing space adventures</p>
+                    
+                    <div className="gallery__grid">
+                        {galleryImages.map((image) => (
+                            <div key={image.id} className="gallery__item">
+                                <a 
+                                    href={image.src} 
+                                    data-lightbox="gallery"
+                                    data-title={image.title}
+                                    className="gallery__link"
+                                >
+                                    <img 
+                                        src={image.src} 
+                                        alt={image.alt}
+                                        className="gallery__image"
+                                        loading="lazy"
+                                    />
+                                    <div className="gallery__overlay">
+                                        <span className="gallery__view-icon">👁️</span>
+                                    </div>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    
+                    <div className="gallery__view-more">
+                        <a href="#" className="gallery__view-more-link body-large">View more</a>
+                    </div>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
 
 export default Gallery;
---
+++ src/utils/gallery.js
@@ -0,0 +1,45 @@
+import image1 from '../assets/images/gallery/image1.png';
+import image2 from '../assets/images/gallery/image2.png';
+import image3 from '../assets/images/gallery/image3.png';
+import image4 from '../assets/images/gallery/image4.png';
+import image5 from '../assets/images/gallery/image5.png';
+import image6 from '../assets/images/gallery/image6.png';
+
+export const galleryImages = [
+    {
+        id: 1,
+        src: image1,
+        alt: 'Futuristic spacecraft flying above Earth',
+        title: 'Spacecraft above Earth'
+    },
+    {
+        id: 2,
+        src: image2,
+        alt: 'Spacecraft in front of Saturn',
+        title: 'Spacecraft near Saturn'
+    },
+    {
+        id: 3,
+        src: image3,
+        alt: 'Spacecraft on Mars surface',
+        title: 'Landing on Mars'
+    },
+    {
+        id: 4,
+        src: image4,
+        alt: 'Astronaut on Mars',
+        title: 'Astronaut on Mars'
+    },
+    {
+        id: 5,
+        src: image5,
+        alt: 'Astronauts celebrating',
+        title: 'Astronauts celebrating'
+    },
+    {
+        id: 6,
+        src: image6,
+        alt: 'Astronauts setting up camp',
+        title: 'Setting up camp'
+    }
+]
